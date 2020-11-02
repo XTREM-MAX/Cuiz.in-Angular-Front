fec2721b726d628e78b5bed4394acbfffb1c210e
leomelki <melkileo@gmail.com>
2020-11-02 21:50:58 +0100

    Recipes now fade with no delay when added after the interface has been initialized

--- src/app/views/recipes/recipe/recipe.component.ts
+++ src/app/views/recipes/recipe/recipe.component.ts
@@ -1,19 +1,17 @@
 import { Recipe } from './../../../models/recipes/Recipe';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.scss']
 })
-export class RecipeComponent implements OnInit {
+export class RecipeComponent {
 
   @Input() recipe: Recipe;
   @Input() index: number;
+  @Input() delayAnimation: boolean;
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
