16663fa4c9e76d70e1e57f5225703f88a5bbc3f6
leomelki <melkileo@gmail.com>
2020-11-06 00:54:34 +0100

    Avoid touch default browser actions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Cuiz.in';
   constructor(public router: Router) {
+    document.addEventListener('touchmove', e => { e.preventDefault(); return false }, { passive: false });//Avoid touch default browser actions
     document.addEventListener('contextmenu',  e => e.preventDefault()); //Avoid right clicking & long press
   }
 }
