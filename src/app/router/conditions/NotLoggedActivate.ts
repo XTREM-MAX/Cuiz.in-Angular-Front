10b71585aa66c566301c56fcf87917ab14df7a35
leomelki <melkileo@gmail.com>
2020-11-16 15:48:34 +0100

    ClientService > Login errors

--- src/app/router/conditions/NotLoggedActivate.ts
+++ src/app/router/conditions/NotLoggedActivate.ts
@@ -7,7 +7,7 @@ export class NotLoggedActivate implements CanActivate {
   constructor(private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>|Promise<boolean>|boolean {
-    let logged = true;
+    let logged = false;
     if (logged)
       this.router.navigateByUrl('/home');
     return !logged;//TODO not logged
