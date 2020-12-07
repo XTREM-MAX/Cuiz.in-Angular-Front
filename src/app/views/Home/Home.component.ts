3d8e3d1eb809b7d6d1b167c969b45cd4cbb6bd3b
leomelki <melkileo@gmail.com>
2020-12-07 17:07:26 +0100

    Home > Swipe

--- src/app/views/Home/Home.component.ts
+++ src/app/views/Home/Home.component.ts
@@ -10,8 +10,17 @@ export class HomeComponent implements AfterViewInit {
 
   public parseInt = window.parseInt;
 
+  transitioning: boolean;
+
   @ViewChild("view")
   public view: ElementRef;
+  
+  @ViewChild("lastcard")
+  public lastcard: ElementRef;
+  @ViewChild("secondcard")
+  public secondcard: ElementRef;
+  @ViewChild("firstcard")
+  public firstcard: ElementRef;
 
   constructor(public client: ClientService) { }
 
@@ -47,13 +56,12 @@ export class HomeComponent implements AfterViewInit {
           pressedElement.style.transform = "translateX(400px) scale(.95) rotateZ(5deg)";
           pressedElement.style.opacity = ".01";
           //like
-          this.client.likeRecipe(this.client.homeRecipe.recipe.nameSlugify);
+          this.loadNewCard(true);
         }else if (event.clientX - started.x < -100) {
           pressedElement.style.transition = "transform .3s cubic-bezier(.3,.83,.52,.92), opacity .3s";
           pressedElement.style.transform = "translateX(-400px) scale(.95) rotateZ(-5deg)";
           pressedElement.style.opacity = ".01";
-          //like
-          this.client.likeRecipe(this.client.homeRecipe.recipe.nameSlugify);
+          this.loadNewCard(false);
         } else {
           pressedElement.style.transition = pressedElement.style.transform = "";
         }
@@ -66,6 +74,37 @@ export class HomeComponent implements AfterViewInit {
         pressedElement.style.transform = `translateX(${(diff > 0 ? 2 : -2) * Math.pow(Math.abs(diff), .5)}px) rotateZ(${(diff > 0 ? 2 : -2)/20  * Math.pow(Math.abs(diff), .5)}deg) scale(${1 - 2/2000 * Math.pow(Math.abs(diff), .5)})`;
       }
     });
+  }
+
+  loadNewCard(liked: boolean) {
+    setTimeout(() => {
+    this.transitioning = true;
+      this.firstcard.nativeElement.style.transform = this.firstcard.nativeElement.style.transition = this.firstcard.nativeElement.style.opacity = "";
+      this.firstcard.nativeElement.className = "end notransition";
+      this.secondcard.nativeElement.className = "second notransition";
+      this.lastcard.nativeElement.className = "last notransition";
+      setTimeout(() => {
+        this.firstcard.nativeElement.className = "last notransition";
+        this.secondcard.nativeElement.className = "";
+        this.lastcard.nativeElement.className = "second";
+        setTimeout(() => {
+          this.firstcard.nativeElement.className = "notransition";
+          this.firstcard.nativeElement.style.transformOrigin = "center"
+          this.firstcard.nativeElement.style.transform = "rotateY(180deg)"
+          this.secondcard.nativeElement.className = "second notransition";
+          this.lastcard.nativeElement.className = "last notransition";
+          this.transitioning = false;
+          setTimeout(() => {
+            this.firstcard.nativeElement.style.transition = "transform .5s cubic-bezier(.21,.96,.52,1.47)"
+            this.firstcard.nativeElement.className = "";
+            this.firstcard.nativeElement.style.transform = ""
+            setTimeout(() => {
+              this.firstcard.nativeElement.style.transition = this.firstcard.nativeElement.style.transformOrigin = "";
+            }, 450);
+          }, 50);
+        }, 300);
+      }, 10);
+    }, 400);
   }
 
 }
